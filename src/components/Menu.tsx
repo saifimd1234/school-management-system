da6dc0619283a79b6c1848c54b6d0916969b6d13
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-17 21:55:17 +0530

    Add the menu icons, names and style them.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -117,7 +120,27 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                >
+                  <Image src={item.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+          })}
+        </div>
+      ))}
+    </div>
   )
 }
 
