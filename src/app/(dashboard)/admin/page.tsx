628387ac7f13cbc751f6ba0cc37764a277591b79
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-17 22:38:50 +0530

    Create the AdminPage basic sections.

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const AdminPage = () => {
     return (
-        <div>AdminPage</div>
+        <div className='p-4 flex gap-4 flex-col md:flex-row'>
+            {/* LEFT */}
+            <div className='text-black w-full lg:w-2/3 bg-red-200'>l</div>
+            {/* RIGHT */}
+            <div className='w-full lg:w-1/3 bg-blue-200'>r</div>
+        </div>
     )
 }
 
