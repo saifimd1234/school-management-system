347f18b9cbf68d9958edcb5765c3d93b539fc133
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-17 21:11:54 +0530

    Basic layout screen of each (dashboard) routing page.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,6 +4,11 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>dashboard{children}</div>
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[20%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-cyan-200"></div>
+      {/* RIGHT */}
+      <div className="w-[80%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col"></div>
+    </div>
   );
 }
