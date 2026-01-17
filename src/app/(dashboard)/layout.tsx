00f0e6324b43020aa73ded821a92c3d0ee4a8c5b
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-17 21:14:43 +0530

    Make the left menu bar heading.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,7 +9,15 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[20%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-cyan-200"></div>
+      <div className="w-[20%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-cyan-200">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold">SchooLama</span>
+        </Link>
+      </div>
       {/* RIGHT */}
       <div className="w-[80%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col"></div>
     </div>
